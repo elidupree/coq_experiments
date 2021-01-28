a74ee9c96217e40f5efde94f5fe1fbc13a047a68
Eli Dupree <vcs@elidupree.com>
2021-01-27 22:16:42 -0500

    more work on effect system

--- EffectSystem2.v
+++ EffectSystem2.v
@@ -73,4 +73,121 @@ Qed.
 Lemma NullStepsIrrelevant A (SA : Step A) (NonNullSA : Step A) (null_dec : ∀ x y : A, SA x y → NonNullSA x y ∨ x = y) (R : A → Prop) i : (∀ f, History NonNullSA i f → R f) → (∀ f, History SA i f → R f).
   intros; exact (H f (RemoveNullSteps NonNullSA null_dec H0)).
 Qed.
+
+
+
+
+
+Definition MemoryValue := nat.
+Definition Address := nat.
+Definition ThreadId := nat.
+
+(* ThreadClaimed doesn't refer to explicit ownership, but to what's implied *must* be the ownership given that a thread did a nonatomic operation at the address and it wasn't known to be forbidden at the time. This doesn't account for shared read access yet (just because I don't need it yet).
+
+After a thread does a nonatomic operation, other threads can't mess with it until the first thread does an SeqCst operation AND the second thread does a SeqCst operation. So, to know what threads would be allowed to start doing nonatomic operations, we have to remember *which* threads have done SeqCst operations since the original SeqCst. *)
+Inductive AddressState := 
+| ThreadClaimed : ThreadId → MemoryValue → AddressState
+| Released : (ThreadId → Prop) → MemoryValue → AddressState.
+
+Definition ThreadCanNonAtomicAccess id state : Prop := match state with
+| ThreadClaimed id2 _ => id = id2
+| Released allowed _ => allowed id
+end.
+
+Definition AddressValue state : MemoryValue := match state with
+| ThreadClaimed _ value => value
+| Released _ value => value
+end.
+
+Definition AddressAfterSeqCst id value state : AddressState := match state with
+| ThreadClaimed _ _ => Released (eq id) value
+| Released allowed _ => Released (λ id2, allowed id2 ∨ id = id2) value
+end.
+
+Inductive PrimitiveOps : Type → Type :=
+| NonAtomicRead : Address → PrimitiveOps nat
+| NonAtomicWrite : Address → MemoryValue → PrimitiveOps unit
+| CompareAndSwapSeqCst : Address → MemoryValue → MemoryValue → PrimitiveOps bool
+| StoreSeqCst : Address → MemoryValue → PrimitiveOps unit
+| Join : ThreadId → PrimitiveOps unit
+| Spawn : ThreadControlFlow → PrimitiveOps ThreadId
+with ThreadControlFlow := 
+| Return : ∀ ReturnType : Type, ReturnType → ThreadControlFlow
+| PrimitiveThen : ∀ po, PrimitiveOps po → (po → ThreadControlFlow) → ThreadControlFlow
+| PrimitiveLoop : ∀ po, PrimitiveOps po → (po → option ThreadControlFlow) → ThreadControlFlow.
+
+Definition Memory := (Address → AddressState).
+
+Record ConcreteState := mkState
+  { stateMemory : Memory
+  ; stateThreads : list (ThreadId × ThreadControlFlow)
+  }.
+Locate eq_dec.
+(* Definition NonThreadRelatedPrimitiveStep po (id : ThreadId) (oldMemory : Memory) (primitive : PrimitiveOps po) (newMemory : Memory) (newControlFlow : ThreadControlFlow) : Prop :=
+match primitive with
+| NonAtomicRead address => ThreadCanNonAtomicAccess id (memory address) ∧ ∃ continuation , newControlFlow = PrimitiveThen (NonAtomicRead address) continuation ∧ 
+| NonAtomicWrite address value : Address → MemoryValue → PrimitiveOps unit
+| CompareAndSwapSeqCst : Address → MemoryValue → MemoryValue → PrimitiveOps bool
+| StoreSeqCst : Address → MemoryValue → PrimitiveOps unit
+| _ : False
+end
+
+→ ThreadControlFlow → Memory →  ThreadControlFlow → Prop :=
+| NonAtomicReadValid : ∀ memory address continuation, ThreadCanNonAtomicAccess id (memory address) → NonSpawnPrimitiveStep id memory (PrimitiveThen (NonAtomicRead address) continuation) (continuation (AddressValue (memory address)))
+| NonAtomicReadInvalid : ∀ memory address continuation whatever, ¬ ThreadCanNonAtomicAccess id (memory address) → NonSpawnPrimitiveStep id memory (PrimitiveThen (NonAtomicRead address) continuation) whatever.
+Inductive NonSpawnPrimitiveStep (id : ThreadId) : Memory → ThreadControlFlow → Memory →  ThreadControlFlow → Prop :=
+| NonAtomicReadValid : ∀ memory address continuation, ThreadCanNonAtomicAccess id (memory address) → NonSpawnPrimitiveStep id memory (PrimitiveThen (NonAtomicRead address) continuation) (continuation (AddressValue (memory address)))
+| NonAtomicReadInvalid : ∀ memory address continuation whatever, ¬ ThreadCanNonAtomicAccess id (memory address) → NonSpawnPrimitiveStep id memory (PrimitiveThen (NonAtomicRead address) continuation) whatever. *)
+
+Require Import Arith.PeanoNat.
+Import Nat.
+Definition setAddress address memory newState : Memory :=
+  (λ address2, if eq_dec address address2 then newState else memory address2).
   
+Inductive Selection A : list A → Type :=
+| selectHead : ∀ x l, Selection (x :: l)
+| selectNext : ∀ x l, Selection l → Selection (x :: l).
+
+Fixpoint selectedValue A (l : list A) (s : Selection l) : A := match s with
+| selectHead x l => x
+| selectNext x l sl => selectedValue sl
+end.
+
+Fixpoint replaceSelected A (l : list A) (s : Selection l) (new : A) : list A := match s with
+| selectHead x l => new :: l
+| selectNext x l sl => x :: replaceSelected sl new
+end.
+
+Definition PrimitiveThenStep (prev next : ConcreteState) (thread : Selection (stateThreads prev)) po (p : PrimitiveOps po) (cont : po → ThreadControlFlow) : Prop :=
+  let memory := stateMemory prev in let (id , control) := selectedValue thread in
+    match p in PrimitiveOps po return (po → ThreadControlFlow) → Prop with
+      | NonAtomicRead address => λ cont, let value := AddressValue (memory address) in
+        ¬ ThreadCanNonAtomicAccess id (memory address) ∨ next = mkState
+          (setAddress address memory (ThreadClaimed id value))
+          (replaceSelected thread (id, cont value))
+      | NonAtomicWrite address value => λ cont, 
+        ¬ ThreadCanNonAtomicAccess id (memory address) ∨ next = mkState
+          (setAddress address memory (ThreadClaimed id value))
+          (replaceSelected thread (id, cont tt))
+      | StoreSeqCst address value => λ cont, next = mkState
+          (setAddress address memory (AddressAfterSeqCst id value (memory address)))
+          (replaceSelected thread (id, cont tt))
+      | CompareAndSwapSeqCst address old new => λ cont, let value := AddressValue (memory address) in
+          next = 
+            if eq_dec old value then
+              mkState
+                (setAddress address memory (AddressAfterSeqCst id new (memory address)))
+                (replaceSelected thread (id, cont true))
+            else
+              mkState memory (replaceSelected thread (id, cont false))
+      | _ => λ cont, False
+      end cont.
+
+Definition ConcreteStep : Step ConcreteState :=
+  λ prev next, let memory := stateMemory prev in ∃ thread : Selection (stateThreads prev), let (id , control) := selectedValue thread in match control with
+    | Return _ _ => False
+    | PrimitiveThen _ p cont => PrimitiveThenStep prev next thread p cont
+    | PrimitiveLoop _ p cont => PrimitiveThenStep prev next thread p (λ output, match cont output with Some a => a | None => control end)
+    end.
+
+
