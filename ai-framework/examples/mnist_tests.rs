e2ff3eedb94a1a30929b38ac2a3900501f92b0b0
elidupree <vcs@elidupree.com>
2023-02-20 18:40:05 -0500

    threw together some SGD and stuff

--- ai-framework/examples/mnist_tests.rs
+++ ai-framework/examples/mnist_tests.rs
@@ -5,13 +5,16 @@ use ai_framework::differentiable_operations::{
 };
 use ai_framework::graph;
 use ai_framework::model_1::{
-    calculate_loss, loss_graph_observed_output_variable_id, loss_output_id, InputOutputSampleBatch,
-    Optimizer,
+    calculate_loss, do_inference, loss_graph_observed_output_variable_id, loss_output_id,
+    InputOutputSampleBatch, Optimizer, ValueMaybeBatch, VariableId,
 };
-use ai_framework::optimizers_1::{AdaptiveGradientDescent, NaiveGradientDescent};
+use ai_framework::optimizers_1::{AdaptiveGradientDescent, AdaptiveSGD, NaiveGradientDescent};
 use map_macro::map;
-use ndarray::{s, Array2};
+use ndarray::{s, Array2, Axis};
+use ordered_float::OrderedFloat;
+use std::collections::HashMap;
 use std::env::args;
+use std::iter::zip;
 
 mod mnist_data;
 
@@ -19,7 +22,7 @@ fn main() {
     live_prop_test::initialize();
     let path = args().collect::<Vec<_>>()[1].clone();
     let ((x_train, y_train), (x_test, y_test)) = mnist_data::load(path);
-    let n = 1000;
+    let n = 60000; //1000;
     let x_train = x_train.slice(s![..n, ..]).to_owned().into_dyn();
     let y_train = y_train.slice(s![..n, ..]).to_owned().into_dyn();
     println!(
@@ -47,7 +50,7 @@ fn main() {
 
     let test_samples = InputOutputSampleBatch {
         inputs: map! {image_variable.to_owned() => x_test},
-        outputs: map! {output.to_owned() => y_test},
+        outputs: map! {output.to_owned() => y_test.clone()},
         output_loss_graph,
     };
 
@@ -57,7 +60,12 @@ fn main() {
 
     let mut parameter_value = Array2::zeros((28 * 28, 10)).into_dyn();
     let lg = graph.compose(&train_samples.output_loss_graph);
-    let mut optimizer = AdaptiveGradientDescent { learning_rate: 1.0 };
+    //let mut optimizer = NaiveGradientDescent { learning_rate: 0.006 };
+    //let mut optimizer = AdaptiveGradientDescent { learning_rate: 1.0 };
+    let mut optimizer = AdaptiveSGD {
+        learning_rate: 1.0,
+        batch_size: 200,
+    };
     for iteration in 0..100000 {
         optimizer.step(
             map! {"weights".to_owned() => &mut parameter_value},
@@ -65,18 +73,44 @@ fn main() {
             &lg,
         );
 
-        let train_loss = calculate_loss(
-            map! {"weights".to_owned() => & parameter_value},
-            &lg,
-            &train_samples,
-        );
+        if iteration % 20 == 0 {
+            let train_loss = calculate_loss(
+                map! {"weights".to_owned() => & parameter_value},
+                &lg,
+                &train_samples,
+            );
 
-        let test_loss = calculate_loss(
-            map! {"weights".to_owned() => & parameter_value},
-            &lg,
-            &test_samples,
-        );
-        println!("{iteration}:\n  Train loss: {train_loss}\n  Test loss:{test_loss}");
+            let test_loss = calculate_loss(
+                map! {"weights".to_owned() => & parameter_value},
+                &lg,
+                &test_samples,
+            );
+
+            let mut variable_values: HashMap<VariableId, ValueMaybeBatch> =
+                test_samples.variable_values_including_observed_outputs();
+            variable_values.extend(
+                map! {"weights".to_owned() => & parameter_value}
+                    .iter()
+                    .map(|(id, &val)| (id.clone(), val.clone())),
+            );
+            let test_results = do_inference(&graph, &variable_values).outputs[output].clone();
+            let mut num_correct = 0;
+            for (&given, predictions) in zip(&y_test, test_results.axis_iter(Axis(0))) {
+                let guessed = predictions
+                    .iter()
+                    .enumerate()
+                    .max_by_key(|(i, x)| OrderedFloat(**x))
+                    .unwrap()
+                    .0;
+                let given = given as usize;
+                if given == guessed {
+                    num_correct += 1;
+                }
+            }
+            let accuracy = num_correct as f32 / y_test.len_of(Axis(0)) as f32;
+
+            println!("{iteration}:\n  Train loss: {train_loss}\n  Test loss:{test_loss}\n  Test accuracy: {accuracy}");
+        }
     }
     println!("Done!")
 }
--- ai-framework/src/optimizers_1.rs
+++ ai-framework/src/optimizers_1.rs
@@ -2,6 +2,9 @@ use crate::differentiable_operations::get_only_value;
 use crate::model_1::{
     backprop, do_inference, loss_output_id, Graph, Optimizer, ValueMaybeBatch, VariableId,
 };
+use autograd::rand;
+use autograd::rand::distributions::uniform::SampleRange;
+use ndarray::{stack, Axis};
 use std::collections::HashMap;
 
 pub struct NaiveGradientDescent {
@@ -62,6 +65,81 @@ impl Optimizer for AdaptiveGradientDescent {
             self.learning_rate /= 2.0;
         } else {
             self.learning_rate *= 1.1;
+        }
+    }
+}
+
+pub struct AdaptiveSGD {
+    pub learning_rate: f32,
+    pub batch_size: usize,
+}
+
+// impl AdaptiveSGD {
+//     fn new(learning_rate: f32, batch_size: usize) {
+//         AdaptiveSGD {
+//             learning_rate,
+//             batch_size,
+//         }
+//     }
+// }
+
+impl Optimizer for AdaptiveSGD {
+    fn step(
+        &mut self,
+        mut parameters: HashMap<VariableId, &mut ValueMaybeBatch>,
+        variables_being_held_constant: HashMap<VariableId, ValueMaybeBatch>,
+        loss_graph: &Graph,
+    ) {
+        let n = variables_being_held_constant
+            .values()
+            .next()
+            .unwrap()
+            .shape()[0];
+        let samples: Vec<usize> = (0..self.batch_size)
+            .map(|_| (0..n).sample_single(&mut rand::thread_rng()))
+            .collect();
+        let old_parameters: HashMap<VariableId, ValueMaybeBatch> = parameters
+            .iter()
+            .map(|(id, val)| (id.clone(), (**val).clone()))
+            .collect();
+        let variables_being_held_constant: HashMap<VariableId, ValueMaybeBatch> =
+            variables_being_held_constant
+                .iter()
+                .map(|(k, v)| {
+                    (
+                        k.clone(),
+                        stack(
+                            Axis(0),
+                            &samples
+                                .iter()
+                                .map(|&i| v.index_axis(Axis(0), i))
+                                .collect::<Vec<_>>(),
+                        )
+                        .unwrap(),
+                    )
+                })
+                .collect();
+        let mut variable_values: HashMap<VariableId, ValueMaybeBatch> =
+            variables_being_held_constant.clone();
+        variable_values.extend(parameters.iter().map(|(k, v)| (k.clone(), (**v).clone())));
+        let inference_result = do_inference(loss_graph, &variable_values);
+        let old_loss = *get_only_value(&inference_result.outputs[&loss_output_id()]);
+        let gradients = backprop(loss_graph, &variable_values, &inference_result);
+        for (id, value) in &mut parameters {
+            **value -= &(gradients.variables[id].clone() * self.learning_rate);
+        }
+        let mut new_variable_values: HashMap<VariableId, ValueMaybeBatch> =
+            variables_being_held_constant;
+        new_variable_values.extend(parameters.iter().map(|(k, v)| (k.clone(), (**v).clone())));
+        let new_inference_result = do_inference(loss_graph, &new_variable_values);
+        let new_loss = *get_only_value(&new_inference_result.outputs[&loss_output_id()]);
+        if new_loss > old_loss {
+            for (id, p) in &mut parameters {
+                **p = old_parameters[id].clone();
+            }
+            self.learning_rate /= 2.0;
+        } else {
+            self.learning_rate *= 1.03;
         }
     }
 }
