db961419af5aafe5e758b3c8506d21ed83c1417a
elidupree <vcs@elidupree.com>
2024-11-29 21:54:27 -0500

    More work on IC

--- IntrospectiveCalculus13.v
+++ IntrospectiveCalculus13.v
@@ -68,3 +68,36 @@ Bind Scope list_scope with Location.
 Notation "'𝕃'" := wc_left.
 Notation "'ℝ'" := wc_right.
 Hint Extern 5 => progress change (list WhichChild) with Location in *; shelve : simplify.
+
+(****************************************************
+                   ContextSet
+****************************************************)
+
+Notation "P '⊆₂' Q" := (∀ x y, P x y -> Q x y) (at level 70, no associativity) : type_scope.
+Notation "P '≡₂' Q" := (∀ x y, P x y <-> Q x y) (at level 70, no associativity) : type_scope.
+
+Definition Context M := Location -> M -> Prop.
+Definition ContextSet := ∀ M, Context M -> Prop.
+(* Definition ContextHasSubtreeAt M (x : Location) (C S : Context M) : Prop
+  := ∀ d m, S d m -> C (x++d) m. *)
+  
+(* Definition ContextSet_Positive (Cs : ContextSet) :=
+  ∀ M C1 C2, C1 ⊆₂ C2 -> Cs M C1 -> ∃ C3, C2 ⊆₂ C3 ∧ Cs M C3. *)
+Definition ContextSet_Extensional (Cs : ContextSet) :=
+  ∀ M C1 C2, C1 ≡₂ C2 -> Cs M C1 -> Cs M C2. 
+Definition MeaningsCanContainOtherMeanings (Cs : ContextSet) :=
+  ∀ M C1 C2 (Submeanings : M -> Context M),
+    C2 ≡₂ (λ x m, ∀ C3, C3 ⊆₂ C2 -> (MeaningsAgree Submeanings C3) -> C3 x m) ->
+    Cs M C1 ->
+    Cs M C2
+    (* (λ)
+    Cs M C1 ->
+    ∃ C2, Cs M C2 ∧
+      C1 ⊆₂ C2 ∧
+      ∀ x d m n, Submeanings m d n ->
+        C2 x m -> C2 (x++d) n *)
+        .
+Definition ConformsTo x y (Cs : ContextSet) : Prop :=
+  ∀ M (m : M) C, Cs M C -> C x m -> C y m.
+Definition Independent (I : Location -> Prop) (Cs : ContextSet) : Prop :=
+  ∀ M (m : M), ∃ Cs, Cs M Cs ∧ ∀ x, I x -> C x m.
