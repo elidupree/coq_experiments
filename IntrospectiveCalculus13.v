ded08917a5bc5bd376f9cbac1cd0d06e1c8bcf93
elidupree <vcs@elidupree.com>
2024-11-30 20:06:03 -0500

    more work on IC (passing proof of ConformsTo_Valid)

--- IntrospectiveCalculus13.v
+++ IntrospectiveCalculus13.v
@@ -81,7 +81,7 @@ Notation "P '∩₂' Q" := (λ x y, P x y ∧ Q x y) (at level 80, right associa
 Notation "P '∪₂' Q" := (λ x y, P x y ∨ Q x y) (at level 85, right associativity) : type_scope.
 
 Section ContextSet.
-  Definition Context M := Location -> M -> Prop.
+  Definition Context M := M -> Location -> Prop.
   Definition ContextSet := ∀ M, Context M -> Prop.
     
   Section AbandonedApproaches.
@@ -128,8 +128,8 @@ Section ContextSet.
 
   Definition ContainsMappedContext M N (Map : M -> Context N)
     (Cₘ : Context M) (Cₙ : Context N) :=
-    (∀ x d m n, Cₘ x m -> Map m d n ->
-      Cₙ (x++d) n).
+    (∀ x d m n, Cₘ m x -> Map m n d ->
+      Cₙ n (x++d)).
 
   Definition IsMappedContext M N (Map : M -> Context N)
     (Cₘ : Context M) (Cₙ : Context N) :=
@@ -143,20 +143,26 @@ Section ContextSet.
   
   Class CsValid (Cs : ContextSet) := {
         csv_subtrees : ContextSet_DoesntTreatMeaningsDifferentlyFromSubtrees Cs
-      ; csv_full : ∀ M C, (∀ x m, C x m) -> Cs M C
+      (* ; csv_full : ∀ M C, (∀ m x, C m x) -> Cs M C *)
     }.
   
-  Inductive SameLinealRelation (x y xo yo : Location) : 
+  (* Inductive SameLinealRelation (x y xo yo : Location) : 
   (* Location -> Location -> Location -> Location ->  *)
   Prop :=
-    slr_cons d e : x++d = xo++e -> y++d = yo++e -> SameLinealRelation x y xo yo.
+    slr_cons d e : x++d = xo++e -> y++d = yo++e -> SameLinealRelation x y xo yo. *)
     (* cso_cons x m : C (x++d) m -> CsOffset d e C (x++e) m. *)
   (* Definition ConformsTo x y : ContextSet :=
     λ M C, ∀ d m, C (x++d) m -> C (y++d) m. *)
+  (* Definition ConformsTo x y : ContextSet :=
+    λ M C, ∀ m xo yo, SameLinealRelation x y xo yo -> C xo m -> C yo m. *)
+  Inductive MemberAtLinealRelation S (x d e : Location) : 
+  (* Location -> Location -> Location -> Location ->  *)
+  Prop :=
+    mlr_cons xo : S xo -> x++d = xo++e -> MemberAtLinealRelation S x d e.
   Definition ConformsTo x y : ContextSet :=
-    λ M C, ∀ m xo yo, SameLinealRelation x y xo yo -> C xo m -> C yo m.
-  Definition Independent (I : Location -> Prop) (Cs : ContextSet) : Prop :=
-    ∀ M (m : M), ∃ C, Cs M C ∧ ∀ x, I x -> C x m.
+    λ M C, ∀ m d e, MemberAtLinealRelation (C m) x d e -> MemberAtLinealRelation (C m) y d e.
+  (* Definition Independent (I : Location -> Prop) (Cs : ContextSet) : Prop :=
+    ∀ M (m : M), ∃ C, Cs M C ∧ ∀ x, I x -> C x m. *)
   
   Section Properties.
     Lemma CsValid_union A B : CsValid A -> CsValid B -> CsValid (A ∪₂ B).
@@ -167,11 +173,11 @@ Section ContextSet.
         destruct H0; [left|right]; [destruct CVA|destruct CVB];
           apply (csv_subtrees0 M N C1 C2 Submeanings); assumption.
       }
-      {
+      (* {
         intros.
         left.
         apply csv_full; assumption.
-      }
+      } *)
     Qed.
     
     (* Doesn't work because "union of nothing" doesn't have the "full" case *)
@@ -204,58 +210,55 @@ Section ContextSet.
         split; [destruct CVA|destruct CVB];
           apply (csv_subtrees0 M N C1 C2 Submeanings); assumption.
       }
-      {
+      (* {
         intros. split; apply csv_full; assumption.
-      }
+      } *)
     Qed.
 
     Lemma ConformsTo_Valid x y : CsValid (ConformsTo x y).
       constructor.
       {
         red; intros.
-        intros n xo yo xyo C2xn.
+        intros n d e Mxde.
+        destruct Mxde as (xo, C2nxo, xdxe).
 
         (* destruct xyo. *)
         red in H.
-        pose (proj1 (H _ _) C2xn) as HX; clearbody HX.
+        pose (proj1 (H _ _) C2nxo) as HX; clearbody HX.
 
         (* assert (∃ xm ym f m, Submeanings m f n ∧ xm ++ f = x ∧ ym ++ f = y ∧ C1 xm m). *)
-        assert (∃ xm m f, Submeanings m f n ∧ xm ++ f = xo) as exh.
+        (* assert (∃ xm m f, Submeanings m n f ∧ xm ++ f = xo) as exh. *)
+        assert (∃ xb m f, C1 m xb ∧ Submeanings m n f ∧ xb ++ f = xo) as exh.
         {
           apply HX.
           red; intros.
 
           exists x0.
           exists m.
-          exists d.
-          split; trivial.
+          exists d0.
+          intuition idtac; trivial.
         }
-        destruct exh as (xm, (m, (f, (Smfn, xmfx)))).
+        destruct exh as (xb, (m, (f, (C1mxb, (Smfn, xbfx))))).
         red in H0.
+        specialize (H0 m d (f ++ e)).
         lapply H0.
-
-
-
-        apply H; intros.
-        red in H0.
-        red in H3.
-        red in H.
-
-        (* red in H0.
-        red in H. *)
-        pose (proj1 (H (x ++ d) n) H1) as e; clearbody e.
-
-        apply H; intros.
-        specialize (e Cₙ₂ H2).
-
-        red in H2.
-        
-        eapply (H2 y d _ n).
-        (* apply H in H1. *)
+        {
+          intros (yb, C1myb, ydye).
+          apply mlr_cons with (yb ++ f).
+          refine (proj2 (H _ _) _); trivial.
+          intros.
+          (* red in H1. *)
+          exact (H1 _ _ _ _ C1myb Smfn).
+          rewrite <- app_assoc; assumption.
+        }
+        apply mlr_cons with xb; trivial.
+        rewrite app_assoc.
+        rewrite xbfx.
+        assumption.
       }
-      {
+      (* {
         intros. split; apply csv_full; assumption.
-      }
+      } *)
     Qed.
       
   End Properties.
