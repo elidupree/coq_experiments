d5773acdd31f1a61831eec15786eded86c08f041
Eli Dupree <vcs@elidupree.com>
2025-01-01 09:57:26 -0500

    fixed HigherDestruct -> False

--- Scratchpad.v
+++ Scratchpad.v
@@ -61,17 +61,29 @@ Parameter q_0 : OpaqueFormula.
 Parameter q_S : OpaqueFormula -> OpaqueFormula.
 
 (* Definition ZeroOrSucc q := ∀ C, (q = q_0 -> C) -> (∀ p, q = (q_S p) -> C) -> C.  *)
-Definition InductiveNat n : Prop :=
+(* Definition InductiveNat n : Prop :=
   ∀ (P : OpaqueFormula → Prop)
     (zero_case : P q_0)
     (succ_case : ∀ m, P m → P (q_S m)),
-    P n.
+    P n. *)
 (* Definition CoinductiveNat n :=
   ∀ C, (∀ State, State -> ()) -> C. *)
 Parameter OpaqueEq : OpaqueFormula -> OpaqueFormula -> Prop.
 Parameter oeq_refl : ∀ q, OpaqueEq q q.
 
-Definition HigherDestruct q := ∀ (zero_case : Prop) (succ_case : OpaqueFormula -> Prop), ((OpaqueEq q q_0) -> zero_case) ∧ (∀ p, (OpaqueEq q (q_S p) -> succ_case p)).
+Definition HigherDestructAllCases q
+  (ZeroCase : OpaqueFormula -> Prop)
+  (SuccCase : OpaqueFormula -> OpaqueFormula -> Prop) :=
+  ((OpaqueEq q q_0) -> ZeroCase q) ∧
+  (∀ p, (OpaqueEq q (q_S p) -> (SuccCase p q))).
+Definition HigherDestruct0 := ∀
+  (ZeroCase : OpaqueFormula -> Prop)
+  (SuccCase : OpaqueFormula -> OpaqueFormula -> Prop),
+  ZeroCase q_0 -> HigherDestructAllCases q_0 ZeroCase SuccCase.
+Definition HigherDestructS p := ∀
+  (ZeroCase : OpaqueFormula -> Prop)
+  (SuccCase : OpaqueFormula -> OpaqueFormula -> Prop),
+  (SuccCase p (q_S p)) -> HigherDestructAllCases (q_S p) ZeroCase SuccCase.
 
 
 (* Definition IsSucc Sn n :=
@@ -79,7 +91,7 @@ Definition HigherDestruct q := ∀ (zero_case : Prop) (succ_case : OpaqueFormula
 Definition PredsAre (P : OpaqueFormula -> Prop) m :=
   ∀ p, IsSucc m p -> P p.  *)
 
-Lemma ind_pred n : (∀ n, InductiveNat n -> HigherDestruct n) -> InductiveNat (q_S n) -> InductiveNat n.
+(* Lemma ind_pred n : (∀ n, InductiveNat n -> HigherDestruct n) -> InductiveNat (q_S n) -> InductiveNat n.
   intros hd iSn.
   unfold InductiveNat; intros.
   apply (iSn (λ m, InductiveNat m ∧ ∀ p (is_succ : OpaqueEq m (q_S p)), InductiveNat p)).
@@ -111,40 +123,43 @@ Lemma ind_pred n : (∀ n, InductiveNat n -> HigherDestruct n) -> InductiveNat (
   apply oeq_refl.
   assumption.
   assumption.
-Qed.
+Qed. *)
 
 
 Definition StrictInductiveNat n : Prop :=
   ∀ (P : OpaqueFormula → Prop)
-    (zero_case : HigherDestruct q_0 -> P q_0)
-    (succ_case : ∀ m, HigherDestruct (q_S m) -> P m → P (q_S m)),
+    (zero_case : HigherDestruct0 -> P q_0)
+    (succ_case : ∀ m, HigherDestructS m -> P m → P (q_S m)),
     P n.
+
+Lemma sind_0 : HigherDestruct0 -> StrictInductiveNat q_0.
+  unfold StrictInductiveNat; intros; apply zero_case; assumption.
+Qed.
+
+Lemma sind_S n : HigherDestructS n -> StrictInductiveNat n -> StrictInductiveNat (q_S n).
+  unfold StrictInductiveNat; intros; apply succ_case. assumption. apply H0; assumption.
+Qed.
 
 Lemma sind_pred n : StrictInductiveNat (q_S n) -> StrictInductiveNat n.
   intros  iSn.
   unfold StrictInductiveNat; intros.
-  apply (iSn (λ m, StrictInductiveNat m ∧ ∀ p (is_succ : OpaqueEq m (q_S p)), StrictInductiveNat p)).
+  apply (iSn (λ m, StrictInductiveNat m ∧ ∀ p (is_succ : OpaqueEq m (q_S p)), StrictInductiveNat p)); trivial.
   
   {
     split.
-    { unfold StrictInductiveNat; intros; apply zero_case0; assumption. }
+    { apply sind_0; assumption. }
     
     intros.
-    apply (H True _).
+    apply (H (λ _, True) (λ p _, StrictInductiveNat p)).
+    exact I.
     assumption.
   }
   {
-    split.
-    { unfold StrictInductiveNat; intros. apply succ_case0. assumption.
-     destruct H0. apply H0. trivial. assumption. }
-    destruct H0.
+    split; destruct H0.
+    { apply sind_S; assumption. }
+    
     intros.
-    
-    apply (H False _). assumption.
-    
+    apply (H (λ _, False) (λ p _, StrictInductiveNat p)); assumption.
   }
-  apply oeq_refl.
-
-  assumption.
-  assumption.
+  { apply oeq_refl. }
 Qed.
